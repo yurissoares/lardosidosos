997d84892d5851aab1ba67114b6391fc7643066b
yurissoares <yurissoares@outlook.com>
2020-08-05 18:38:34 -0300

    correçao 3

--- src/main/resources/db/migration/V06__sprint1-1_correcoes.sql
+++ src/main/resources/db/migration/V06__sprint1-1_correcoes.sql
@@ -1,1 +1,9 @@
-ALTER TABLE morador add sexo ENUM('M', 'F');
+ALTER TABLE morador MODIFY COLUMN nm_rg varchar(20) not null;
+ALTER TABLE morador MODIFY COLUMN nm_rg_responsavel varchar(20);
+
+ALTER TABLE morador add sexo ENUM('M', 'F') not null;
+
+ALTER TABLE morador MODIFY COLUMN parentesco_responsavel ENUM('PAI', 'IRMAO', 'SOBRINHO', 'FILHO', 'PRIMO', 'GENRO', 'NETO', 'VIZINHO', 'CONHECIDO', 'OUTRO');
+
+ALTER TABLE morador MODIFY tel_responsavel varchar(50);
+ALTER TABLE morador MODIFY nome_responsavel varchar(50);
