2033db496fad23df2617c425d3f0a5a02b4cf1d0
yurissoares <yuri@aluno.ufrb.edu.br>
2020-10-20 17:01:17 -0300

    alteração nos campos do tipo decimal da db

---
+++ src/main/resources/db/migration/V21__altera_casas-decimais.sql
@@ -0,0 +1,9 @@
+ALTER TABLE ficha_admissao MODIFY peso DECIMAL(10,2);
+
+ALTER TABLE ficha_admissao MODIFY altura DECIMAL(10,2);
+
+ALTER TABLE ficha_ev_diaria MODIFY vl_temperatura DECIMAL(10,2);
+
+ALTER TABLE despesa MODIFY preco DECIMAL(10,2);
+
+ALTER TABLE morador_despesa MODIFY preco_total DECIMAL(10,2);
